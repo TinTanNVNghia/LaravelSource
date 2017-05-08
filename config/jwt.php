ee72dbdacd665bc9de6836712a034f1c89bba06a
TinTanNVNghia <nvnghia@tintansoft.com>
2017-05-08 10:32:10 +0700

    test jwt

--- config/jwt.php
+++ config/jwt.php
@@ -21,7 +21,7 @@ return [
     |
     */
 
-    'secret' => env('JWT_SECRET', 'changeme'),
+    'secret' => env('JWT_SECRET', 'B6ubp75sZdPVOiYFHBT9fLbD2F1yZSu2'),
 
     /*
     |--------------------------------------------------------------------------
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+/* Test Jwt Auth */
+Route::group(['middleware' => ['jwt.auth']],function (){
+    Route::get('/api/product',function (){
+        return 'Hello World!';
+    });
+});
